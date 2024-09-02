55e2f3cecb65b19a5215de4760e17ac73ffe51d2
yueye <2384514075@qq.com>
2024-09-02 20:28:15 +0800

    学习github actions-6

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/sdk/OpenAiCodeReview.java
@@ -1,20 +1,21 @@
 package plus.gaga.middleware.sdk;
 
 import com.alibaba.fastjson2.JSON;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import plus.gaga.middleware.sdk.domain.model.ChatCompletionRequest;
 import plus.gaga.middleware.sdk.domain.model.ChatCompletionSyncResponse;
 import plus.gaga.middleware.sdk.domain.model.Model;
 import plus.gaga.middleware.sdk.types.utils.BearerTokenUtils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class OpenAiCodeReview {
 
@@ -22,20 +23,23 @@ public class OpenAiCodeReview {
         System.out.println("测试执行");
 
         // 1. 代码检出
+
+        //输出当前提交与上一个提交之间的差异
         ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD~1", "HEAD");
+        //指定目录
         processBuilder.directory(new File("."));
-
+        //启动进程
         Process process = processBuilder.start();
-
+        //读取差异内容
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         String line;
-
-        StringBuilder diffCode = new StringBuilder();
+        StringBuilder diffCode = new StringBuilder();//差异代码
         while ((line = reader.readLine()) != null) {
             diffCode.append(line);
         }
-
+        //进程退出码
         int exitCode = process.waitFor();
+
         System.out.println("Exited with code:" + exitCode);
 
         System.out.println("diff code：" + diffCode.toString());
@@ -47,10 +51,13 @@ public class OpenAiCodeReview {
 
     private static String codeReview(String diffCode) throws Exception {
 
-        String apiKeySecret = "c78fbacd3e10118ad5649d7a54a3a163.UunYDBxpzeClvSKZ";
+        //用户鉴权的apiKey
+        String apiKeySecret = "f324acfa09bb651124bb4ba4f59485b6.JbRNoQBp4ZC8AWS4";
+        //得到token
         String token = BearerTokenUtils.getToken(apiKeySecret);
 
         URL url = new URL("https://open.bigmodel.cn/api/paas/v4/chat/completions");
+        //HttpURLConnection
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
         connection.setRequestMethod("POST");
@@ -60,13 +67,16 @@ public class OpenAiCodeReview {
         connection.setDoOutput(true);
 
         ChatCompletionRequest chatCompletionRequest = new ChatCompletionRequest();
+        //传入所需调用的模型编码
         chatCompletionRequest.setModel(Model.GLM_4_FLASH.getCode());
-        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>() {
+
+
+        chatCompletionRequest.setMessages(new ArrayList<ChatCompletionRequest.Prompt>(){
             private static final long serialVersionUID = -7988151926241837899L;
 
             {
-                add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码如下:"));
-                add(new ChatCompletionRequest.Prompt("user", diffCode));
+                add(new ChatCompletionRequest.Prompt("user","你是一个很牛逼的编程架构师，请你根据git diff记录，对以下代码做出评审。代码如下:"));
+                add(new ChatCompletionRequest.Prompt("user",diffCode));
             }
         });
 
@@ -77,6 +87,8 @@ public class OpenAiCodeReview {
 
         int responseCode = connection.getResponseCode();
         System.out.println(responseCode);
+
+
 
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String inputLine;
@@ -93,6 +105,51 @@ public class OpenAiCodeReview {
 
         ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
         return response.getChoices().get(0).getMessage().getContent();
+    }
+
+    private static String writeLog(String token,String log) throws Exception{
+
+        //git操作，指定仓库
+        Git git = Git.cloneRepository()
+                .setURI("")
+                .setDirectory(new File("repo"))
+                .setCredentialsProvider(new UsernamePasswordCredentialsProvider(token, ""))
+                .call();
+
+        //推送日期
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateFolderName = simpleDateFormat.format(new Date());
+
+        //日志目录
+        //例如 repo/2024-09-02
+        File dateFolder = new File("repo/" + dateFolderName);
+        if(!dateFolder.exists()){
+            dateFolder.mkdirs();//没有就生成对应的二级目录
+        }
+
+        //日志文件
+        //例如 111111111111.md
+        String fileName = generateRandomString(12) + ".md";
+        File newFile = new File(dateFolder, fileName);
+
+        //往日志中写入内容
+        //往 111111111111.md中写入内容
+        try(FileWriter fileWriter = new FileWriter(newFile)){
+            fileWriter.write(log);
+        }
+
+        //git命令
+        //编写代码提交代码
+        //先在仓库中创建目录
+        git.add().addFilepattern(dateFolderName+ "/" + fileName).call();
+        //和我们直接操作git控制台的commit一个效果
+        git.commit().setMessage("Add new file").call();
+        //提交也需要账户和密码，仓库已经在git对象上指定好了，用户需要在提交时确定【为什么git也指定了用户和密码？】
+        git.push().setCredentialsProvider(new UsernamePasswordCredentialsProvider(token,"")).call();
+    }
+
+    private static String generateRandomString(int length) {
+        return null;
     }
 
 }
